f814268b630250f7c88cd06cf67011976d326af3
DragonerYan <2269938169@qq.com>
2023-05-05 21:55:39 +0800

    使用jasypt对关键信息进行加密

---
+++ src/main/java/com/example/config/JasyptPropertyValueConfig.java
@@ -0,0 +1,14 @@
+package com.example.config;
+
+import org.springframework.beans.factory.config.PropertyOverrideConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptPropertyValueConfig {
+
+    @Bean
+    public PropertyOverrideConfigurer jasyptPropertyOverrideConfigurer() {
+        return new JasyptPropertyValueHandler();
+    }
+}
